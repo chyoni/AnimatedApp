58bfe309e90780b48595d7a0d5e89d3ff5b186e7
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2021-11-07 14:17:01 +0900

    #02 Raw Implement for Animated

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 
-const Container = styled.View``;
-const Box = styled.TouchableOpacity``;
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
 
 export default function App() {
-  return null;
+  const [y, setY] = useState(0);
+  const [intervalID, setIntervalID] = useState(null);
+  const moveUp = () => {
+    const id = setInterval(() => setY((prev) => prev + 10), 10);
+    setIntervalID(id);
+  };
+
+  useEffect(() => {
+    if (y === 200) {
+      clearInterval(intervalID);
+    }
+  }, [y, intervalID]);
+
+  return (
+    <Container>
+      <Box onPress={moveUp} style={{ transform: [{ translateY: y }] }} />
+    </Container>
+  );
 }
--- README.md
+++ README.md
@@ -7,3 +7,5 @@
   ```bash
   npm i styled-component @types/styled-components @types/styled-components-react-native
   ```
+
+- #02 Raw Implement for Animated
